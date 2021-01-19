23bb11fd0ac1369a733d22827d99b3e1792491fe
ichbindev <cmlinac@gmail.com>
2021-01-18 22:05:48 -0600

    update icon and title

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteMetadata: {
-    title: 'Portfolio',
+    title: 'Chris Mlinac - Portfolio',
     author: 'Chris Mlinac',
     description: 'My Portfolio',
   },
@@ -15,7 +15,7 @@ module.exports = {
         background_color: '#663399',
         theme_color: '#663399',
         display: 'minimal-ui',
-        icon: 'src/images/gatsby-icon.png', // This path is relative to the root of the site.
+        icon: 'src/images/icon.png', // This path is relative to the root of the site.
       },
     },
     'gatsby-plugin-sass',
