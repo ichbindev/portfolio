deef52a018cfaefba05a86303efe16f2107952dd
ichbindev <cmlinac@gmail.com>
2021-03-21 21:21:16 -0500

    use dummy page to test email submission

--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -1,14 +1,25 @@
 import React from 'react';
 import { Link } from 'gatsby';
+import { sendEmail } from '../util/api';
 
 import Layout from '../components/layout';
 
-const SecondPage = () => (
+const SecondPage = () => {
+  const onSubmit = evt => {
+    evt.preventDefault();
+    const form = {
+      name: 'testName',
+      email: 'test@e.mail',
+      content: 'so long and thanks for all the fish'
+    };
+    sendEmail(form);
+  };
+
+  return (
   <Layout>
-    <h1>Hi from the second page</h1>
-    <p>Welcome to page 2</p>
-    <Link to="/">Go back to the homepage</Link>
+    <h1>Test</h1>
+    <button type='submit' onClick={onSubmit}>Test</button>
   </Layout>
-);
+)};
 
 export default SecondPage;
