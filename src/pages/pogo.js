77df06df9f2fe8eb6824ca2a245afd2e32dc5b10
ichbindev <cmlinac@gmail.com>
2022-02-06 12:44:37 -0600

    update h2 spacing on pogo page

--- src/pages/pogo.js
+++ src/pages/pogo.js
@@ -4,8 +4,10 @@ import Layout from '../components/layout';
 export function Pogo() {
   return (
     <Layout>
-      <h2>PoGo Friends</h2>
-      <div>Reach out if you'd like to coordinate a time to hit Best Friends.</div>
+      <h2 style={{ letterSpacing: 0 }}>PoGo Friends</h2>
+      <div>
+        Reach out if you'd like to coordinate a time to hit Best Friends.
+      </div>
       <div>Feel free to open gifts for Ultra Friend whenever.</div>
       <div>Discord: @CasAttack#7645</div>
       <div>Phone: +1 (512) 436-0321</div>
