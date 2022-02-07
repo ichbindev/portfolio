1bcba3220d4ab16bc5fec3557eed7f7dc88ea808
ichbindev <cmlinac@gmail.com>
2022-02-07 13:26:43 -0600

    add friendcode page

---
+++ src/pages/fc.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Layout from '../components/layout';
+
+export function FC(props) {
+  return (
+    <Layout>
+      <div style={{ fontSize: '32px', textAlign: 'center' }}>
+        <div>CasAttack512</div>
+        <div>8078 5301 2925</div>
+      </div>
+    </Layout>
+  );
+}
+
+export default FC;
