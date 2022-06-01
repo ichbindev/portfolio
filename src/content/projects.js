4a4afb6738670c1cb86bbaaf96b69cf901a7408a
ichbindev <cmlinac@gmail.com>
2022-06-01 15:33:04 -0500

    add newtritious to project list

--- src/content/projects.js
+++ src/content/projects.js
@@ -2,9 +2,19 @@ import esistblog from '../images/projects/esistblog.png';
 import fiveStarProspects from '../images/projects/fiveStarProspects.png';
 import psych from '../images/projects/psych.png';
 import trainmethod from '../images/projects/trainmethod.png';
+import newtritious from '../images/projects/newtritious.png';
 import whichCat from '../images/projects/whichCat.png';
 
 const projects = [
+  {
+    link: 'http://www.newtritio.us/',
+    image: newtritious,
+    name: 'newtrious',
+    description: 'Newtritious lets you search, save, and share recipes! I mentored a team of bootcamp graduates who did most of the coding on this app. I made sure industry best practices were taught and followed. I also gave detailed feedback on their code via code reviews.',
+    code: 'https://github.com/newtritious/Newtritious',
+    tech: ['React', 'JavaScript', 'Node', 'Express', 'Mongo'],
+    id: 0,
+  },
   {
     link: 'http://joglogs.herokuapp.com/',
     image: trainmethod,
@@ -13,7 +23,7 @@ const projects = [
       "TrainMethod is a webapp that creates a custom running schedule to train for your big run, whether it's a 5k or a marathon.",
     code: 'https://github.com/ichbindev/joglogs/',
     tech: ['React', 'JavaScript', 'Node', 'Express', 'MySQL'],
-    id: 0
+    id: 0,
   },
   {
     link: 'http://www.fivestarprospects.com/',
@@ -23,7 +33,7 @@ const projects = [
       'A NCAA certified tool to help college football coaches find students to recruit. Players can upload their stats and highlight reels and coaches can search for student athletes.',
     code: 'https://github.com/ichbindev/',
     tech: ['JavaScript', 'jQuery', 'Node', 'Express', 'MySQL'],
-    id: 2
+    id: 2,
   },
   {
     link: 'http://www.ichbin.dev/',
@@ -33,7 +43,7 @@ const projects = [
       'Where I write stuff from time to time, also where I chronicled my 100 Days of Code challenge.',
     code: 'https://github.com/ichbindev/ichbindev/',
     tech: ['React', 'JavaScript', 'GraphQL', 'Gatsby', 'Tailwind'],
-    id: 1
+    id: 1,
   },
   {
     link: 'https://github.com/ichbindev/whichCat.js/blob/master/README.md',
@@ -42,8 +52,15 @@ const projects = [
     description:
       'A self-study iOS app able to correctly identify my cats using a machine learning model I trained.',
     code: 'https://github.com/ichbindev/whichCat.js',
-    tech: ['Swift', 'iOS', 'TensorFlow Lite', 'TensorFlow.js', 'Machine Learning', 'Image Classification'],
-    id: 4
+    tech: [
+      'Swift',
+      'iOS',
+      'TensorFlow Lite',
+      'TensorFlow.js',
+      'Machine Learning',
+      'Image Classification',
+    ],
+    id: 4,
   },
   {
     link: 'https://ichbindev.github.io/Psych/',
@@ -53,7 +70,7 @@ const projects = [
       'A little memory game themed with the TV show Psych. Click all nine characters exactly once, get it wrong and you have to start over.',
     code: 'https://github.com/ichbindev/Psych',
     tech: ['React'],
-    id: 3
+    id: 3,
   },
 ];
 
