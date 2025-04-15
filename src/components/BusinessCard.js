38ac0458c4e94c119d0458cd05afd778c9ee0bb0
Chris <cmlinac@gmail.com>
2025-04-15 09:29:04 -0500

    Update BusinessCard.js

--- src/components/BusinessCard.js
+++ src/components/BusinessCard.js
@@ -1,29 +1,11 @@
 import React from 'react';
+import ContactInfo from './ContactInfo';
 
 function BusinessCard() {
   return (
     <article id="business-card" className="active">
       <h2 className="major">Contact Info</h2>
-      <div style={{ margin: '30px 0' }}>
-        <h3>Chris Mlinac</h3>
-        <div>
-          Email: <a href="mailto:chris@mlin.ac">chris@mlin.ac</a>
-        </div>
-        <div>Phone: (512) 436-0321</div>
-        <div>
-          Twitter:{' '}
-          <a href="https://twitter.com/ichbindev" target="_blank">
-            @ichbindev
-          </a>
-        </div>
-        <div>
-          Github:{' '}
-          <a href="https://github.com/ichbindev" target="_blank">
-            ichbindev
-          </a>
-        </div>
-        <div>Discord: @CasAttack#7645</div>
-      </div>
+      <ContactInfo />
     </article>
   );
 }
